be5e2a82c1eced32abd57788b29ce9b8fd56f130
LBubala <90238524+LBubala@users.noreply.github.com>
2025-04-27 14:29:58 +0200

    Update farmer.lua

--- farmer.lua
+++ farmer.lua
@@ -1,16 +1,40 @@
-local colSize = 11
+local colSize = 4
+local rowSize = 11
+local isFloded = true
+local startX, startY, StartZ = 0, 0, 0
+local leftDirection = true
 
 function Start()
    print("Start")
-   forward()
+   startHarwesting()
+end
+
+function startHarwesting()
+   local col
+   row = 1,colSize
+      refuel()
+      forward()
+      chooseDirection()
+   end
 end
 
 function forward()
-
-   refuel()
-
-   for i=1,colSize do
+   for i=1,rowSize do
       turtle.forward()
+   end
+end
+
+function chooseDirection()
+   if leftDirection then
+      turtle.turnLeft()
+      turtle.forward()
+      turtle.turnLeft()
+      leftDirection = false
+   else
+      turtle.turnRight()
+      turtle.forward()
+      turtle.turnRight()
+      leftDirection = true
    end
 end
 
